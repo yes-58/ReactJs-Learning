cef7d24ac4efedb0286919459f5edeff1a31760f
Yash Sethiya <yashsethiya58@gmail.com>
2025-01-02 01:10:05 +0530

    React Basics,Components and Component Composition

--- App.js
+++ App.js
@@ -4,14 +4,13 @@ import ReactDOM from "react-dom";
 const Title = ()=> {
     return <h1 className="title">React Js Component composition : Inner Component</h1>
 }
-
+const name = "Learn React"
 const HeadingComponent = () => {
     return <div className="heading">
         <Title /> 
-        <h1>Outer Component</h1>
+        <h1>Outer Component {name}</h1>
     </div>
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent />)
