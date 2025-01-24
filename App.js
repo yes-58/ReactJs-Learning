4214f18f293e6a03817ba7808a407b00d260711c
Yash <yashsethiya58@gmail.com>
2025-01-25 02:01:21 +0530

    Dynamic Data and Config Driven UI

--- App.js
+++ App.js
@@ -18,14 +18,65 @@ const Header =()=>{
     </div>
     )
 }
-const RestaurantCard=()=>{
+const resList=[{
+    "info": {
+"id": "393840",
+"name": "Chinese Wok",
+"cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+"locality": "Chikka Lakshmaiah Layout",
+"areaName": "Adugodi",
+"costForTwo": "₹250 for two",
+"cuisines": [
+"Chinese",
+"Asian",
+"Tibetan",
+"Desserts"
+],
+"avgRating": 4.3,
+"parentId": "61955",
+"avgRatingString": "4.3",
+"totalRatingsString": "5.3K+",
+"sla": {
+"deliveryTime": 41,
+"lastMileTravel": 2.6,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "2.6 km",
+"iconType": "ICON_TYPE_EMPTY"
+}}},{"info": {
+"id": "10576",
+"name": "Pizza Hut",
+"cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/87e56c63-b521-4257-96ae-a42229b92009_10576.jpg",
+"locality": "6th Block",
+"areaName": "Koramangala",
+"costForTwo": "₹350 for two",
+"cuisines": [
+"Pizzas"," Fast Food"
+],
+"avgRating": 4.2,
+"parentId": "721",
+"avgRatingString": "4.2",
+"totalRatingsString": "21K+",
+"sla": {
+"deliveryTime": 41,
+"lastMileTravel": 0.7,
+"serviceability": "SERVICEABLE",
+"slaString": "40-45 mins",
+"lastMileTravelString": "0.7 km",
+"iconType": "ICON_TYPE_EMPTY"
+}
+}}]
+
+const RestaurantCard=(props)=>{
+    const {resData} = props
+    const {cloudinaryImageId, name, cuisines, avgRating, costForTwo} = resData.info
     return (
         <div className="res-card" style={{backgroundColor:'#f0f0f0'}}>
-            <img className="res-logo" alt="res-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"/>
-            <h3>Meghana Food</h3>
-            <h4>Biryani, North Indian, Asian</h4>
-            <h4>4.4 Stars</h4>
-            <h4>38 minutes</h4>
+            <img className="res-logo" alt="res-image" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}/>
+            <h3>{name}</h3>
+            <h4>{cuisines.join(',')}</h4>
+            <h4>Rating: {avgRating}</h4>
+            <h4>{costForTwo}</h4>
         </div>
 
     )
@@ -38,20 +89,9 @@ const Body =()=>{
                 search
             </div>
             <div className="res-container" >
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
-                <RestaurantCard />
+                {
+                    resList.map((restaurant)=>(<RestaurantCard key={restaurant.info.id} resData = {restaurant} />))
+                }
             </div>
             
         </div>
